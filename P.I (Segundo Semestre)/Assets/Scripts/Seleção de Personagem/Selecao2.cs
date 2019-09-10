e5aff1699c26c47d3b0b03d4e98233f1762d14f6
Guilherme Rodrigo Camblor <guicamblor@gmail.com>
2019-09-10 20:05:46 -0300

    Aaargh
    teste 1

---
+++ P.I (Segundo Semestre)/Assets/Scripts/Seleção de Personagem/Selecao2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selecao2 : MonoBehaviour
+{
+    public GameObject characterSelect;
+
+    private string objName;
+    void Start()
+    {
+
+        switch (characterSelect)
+        {
+
+        }
+    }
+
+    
+    
+}
