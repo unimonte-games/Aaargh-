85420fe3fa343d036db670da854723d946e99c16
Guilherme Rodrigo Camblor <guicamblor@gmail.com>
2019-11-13 23:50:00 -0300

    Teste tentando arrumar erros pesados

---
+++ Assets/Scripts/HUD/Painels.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painels : MonoBehaviour
+{
+    public GameObject painels;
+    private void OnCollisionEnter(Collision collision)
+    {
+        painels.SetActive(true);
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        painels.SetActive(false);
+    }
+    
+}
